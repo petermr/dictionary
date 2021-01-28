c1da420b2617914afa64aab48897be3f8708cfa5
petermr <peter.murray.rust@googlemail.com>
2021-01-28 16:51:14 +0000

    remove bad filename

--- pythoncode/pyamidict/editor/wikimedia.py
+++ pythoncode/pyamidict/editor/wikimedia.py
@@ -9,6 +9,8 @@ from amidict import Resources
 
 # SPARQL keywords
 WIKIDATA_QUERY_URL = 'https://query.wikidata.org/sparql'
+RESULTS_NS = "http://www.w3.org/2005/sparql-results#"
+RESULTS_NS_BR = "{"+RESULTS_NS+"}"
 # query
 FORMAT = 'format'
 QUERY = 'query'
@@ -17,8 +19,10 @@ XSD_DEC = "http://www.w3.org/2001/XMLSchema#decimal"
 DATATYPE = "datatype"
 TYPE = "type"
 LITERAL = "literal"
+NAME = "name"
 URI = "uri"
 VALUE = "value"
+JSON = "json"
 XML_LANG = "xml:lang"
 HEAD = "head"
 VARS = "vars"
@@ -46,7 +50,7 @@ TEST_QUERY = """
         """
 PYAMIDICT, RESOURCE_DIR, DICT202011, TEMP_DIR, DICTIONARY_TOP = Resources().get_resources()
 
-class WikidataPedia():
+class WikimediaLib():
 
     def __init__(self):
 #        print("WP init")
@@ -55,13 +59,12 @@ class WikidataPedia():
     def help(self):
         print("help for Wikimedia routines NYI")
 
-    def read_sparql_xml(self, file):
+    def read_sparql_xml_to_pandas(self, file):
         """<?xml version='1.0' encoding='UTF-8'?>
 <sparql xmlns='http://www.w3.org/2005/sparql-results#'>
 	<head>
 		<variable name='wikidata'/>
 		...
-		<variable name='term'/>
 	</head>
 	<results>
 		<result>
@@ -80,43 +83,49 @@ class WikidataPedia():
         except:
             print("cannot parse XML", file)
             return None
-        result_list = root.findall("./*/{http://www.w3.org/2005/sparql-results#}result")
+        result_list = root.findall("./*/" + RESULTS_NS_BR + RESULT)
         rowdata = []
         for i, result in enumerate(result_list):
-            binding_list = result.findall("./{http://www.w3.org/2005/sparql-results#}binding")
+            binding_list = result.findall("./" + RESULTS_NS_BR + BINDING)
             new_row_dict = {}
             for binding in binding_list:
-                name = binding.attrib["name"]
+                name = binding.attrib[NAME]
                 child = list(binding)[0]
-                type_ = str(child.tag).partition("}")[2]
+                # format is {uri}name so strip uri (may use this later)
+                parts = child.tag.partition("}")
+                tag = parts[2] if child.tag.startswith("{") else child.tag
+                namespace = parts[0][1:] if child.tag.startswith("{") else None
+                if namespace is not None and namespace != RESULTS_NS:
+                    print("unexpected namespace in sparql xml", namespace, "expected", RESULTS_NS)
+                    continue
                 val = child.text
                 # there may be other types of output - don't know
-                if type == "junk":
-                    val = float(val)
-                elif type_ == URI:
+                if tag == URI:
                     pass
-                elif type_ == LITERAL:
+                elif tag == LITERAL:
                     pass
                 else:
-                    print("Cannot parse")
+                    print("unknown tag", tag)
                 new_row_dict[name] = val
             rowdata.append(new_row_dict)
         return pd.DataFrame(rowdata)
-
-
 
     def post_sparql(self, query, format="json"):
         """https://requests.readthedocs.io/en/master/ HTTP for humans"""
         return self.post_request(WIKIDATA_QUERY_URL, query, format)
 
-    def post_request(self, url, query, format="json"):
+    def post_request(self, url, query, format=JSON):
         import requests
         if query is None or len(query) == 0:
             print("empty query")
-            return
-        req = requests.get(url, params={FORMAT: format, QUERY: query})
-        print("req", req)
-        return req.json()
+            return None
+        try:
+            req = requests.get(url, params={FORMAT: format, QUERY: query})
+            print("req", req)
+            return req.json() if format == JSON else None
+        except requests.exceptions.ConnectionError:
+            print("Cannot connect")
+            return None
 
     def test_query_wikipedia(self):
         """for experimenting"""
@@ -127,10 +136,10 @@ class WikidataPedia():
         print("Bear", wp.search("bear"))
         print("reality_summary", wp.summary("reality checkpoint"))
     #    print("pmr_page", wp.page(title="Peter Murray-Rust", preload=True))
-        page = wp.WikipediaPage(title="Chaffinch", preload=True)
+        page = wp.WikipediaPage(title="Ocimum_kilimandscharicum", preload=True)
         print("categories", page.categories,
 ## these are quite large
-#              "\n", "content", page.content,
+              "\n", "content", page.content,
     #          "\n", page.coordinates,
     #          "\n", "html", page.html,
     #          "\n", "images", page.images,
@@ -147,12 +156,15 @@ class WikidataPedia():
 
         *query* SPARQL query (assumed correct)
 
-        return DataFrame (columns from SPARQL SELECT)
+        return DataFrame (columns from SPARQL SELECT) or None if failed
         """
         import pandas as pd
 
-        wm = WikidataPedia();
+        wm = WikimediaLib();
         query_results_dict = wm.post_sparql(query)
+        if query_results_dict is None:
+            print("no results from SPARQL")
+            return None
         # results is a 2-element dictionary with keys = "head" and "results"
         # "head' dict with "vars" child dict as list of column names
         head_dict = query_results_dict[HEAD]
@@ -181,17 +193,41 @@ class WikidataPedia():
             rowdata.append(new_row_dict)
         return pd.DataFrame(rowdata)
 
+    def analyze(self, sparql_xml_file, term="term", desc="wikidataDescription"):
+        df = self.read_sparql_xml_to_pandas(sparql_xml_file)
+        self.analyze_general(df, term=term, desc=desc)
+        self.analyze_specific(df, desc=desc)
+
+    def analyze_general(self, df, desc="description", term="term"):
+        print("\n", "cevspql_df", df, "\n")
+        print("cols", df.columns)
+        print(df[term], df[desc])
+        return df
+
+    def analyze_specific(self, df, desc):
+        region = "region"
+        df[region] = df[desc].str.partition(' in ')[2].str.lower()
+        print(region, df[region])
+        print(df.sort_values(by=[region]))
+        print(df[region].value_counts())
+#        fig, ax = plt.subplots()
+#        df[region].value_counts().plot(ax=ax, kind='bar')
+
 
 def main():
+    import matplotlib.pyplot as plt
     """
     return
     """
-    wm = WikidataPedia()
+    wm = WikimediaLib()
     wm.help()
-    df = wm.submit_process_sparql(query=TEST_QUERY)
-    print("df", df)
-    xml = wm.read_sparql_xml(os.path.join(DICTIONARY_TOP, "openVirus202011/country/work/sparql_final_dict.xml"))
-    print("xml", xml)
+    df0 = wm.submit_process_sparql(query=TEST_QUERY)
+    print("cevspql_df", df0)
+
+    sparql_xml_file = os.path.join(DICTIONARY_TOP, "openVirus202011/country/work/sparql_final_dict.xml")
+    wm.analyze(sparql_xml_file)
+
+
 #    wm.test_query_wikipedia()
     print("end of wikipedia main")
 
