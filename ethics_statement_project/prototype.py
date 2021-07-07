4dcccd1f70bb7e0922cf843b8e83edae574360d4
Shweata N Hegde <shweata.hegde@gmail.com>
2021-07-07 16:10:34 +0530

    testing textacy

--- ethics_statement_project/prototype.py
+++
@@ -1,5 +0,0 @@
-from glob import glob
-import os
-ethics_statements = glob(os.path.join(
-    os.getcwd(), 'ethics_statement_frontiers_100', 'PMC*', 'sections', '**', '[1_9]_p.xml'), recursive=True)
-print(ethics_statements)
